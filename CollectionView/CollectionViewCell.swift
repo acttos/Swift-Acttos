737d4b3c08755b637739322d1863bd9b393152cb
Jason Ma <majinshou@gmail.com>
2016-09-08 20:07:06 +0800

    Add a new 'CollectionView' demo

---
+++ CollectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Swift-Acttos
+//
+//  Created by Acttos on 8/9/2016.
+//  Copyright © 2016 Acttos.org. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var backgroundImageView: UIImageView!
+}
--- CollectionView/ViewController.swift
+++ CollectionView/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +19,41 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    //MARK: - DataSource Methods of UICollectionView
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 3;
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7;
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell: CollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionViewCell", forIndexPath: indexPath) as! CollectionViewCell;
+        
+        if (indexPath.section % 2 == 0) {
+            cell.backgroundImageView.image = UIImage.init(data: NSData(contentsOfURL: NSURL(string: "https://avatars2.githubusercontent.com/u/6056509?v=3&s=460")!)!);
+        } else {
+            cell.backgroundImageView.image = UIImage(named: "wechat.png");
+        }
+        
+        return cell;
+    }
+    //MARK: - Delegate Methods of UICollectionView
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        NSLog("The cell at section : \(indexPath.section), row : \(indexPath.row) has been clicked....");
+    }
+    
+    func collectionView(collectionView: UICollectionView, didHighlightItemAtIndexPath indexPath: NSIndexPath) {
+        let cell: CollectionViewCell = collectionView.cellForItemAtIndexPath(indexPath) as! CollectionViewCell;
+        let frame: CGRect = cell.backgroundImageView.frame;
+        cell.backgroundImageView.frame = CGRectMake(frame.origin.x + 4, frame.origin.y + 4, frame.size.width - 8, frame.size.height - 8);
+    }
+    
+    func collectionView(collectionView: UICollectionView, didUnhighlightItemAtIndexPath indexPath: NSIndexPath) {
+        let cell: CollectionViewCell = collectionView.cellForItemAtIndexPath(indexPath) as! CollectionViewCell;
+        let frame: CGRect = cell.backgroundImageView.frame;
+        cell.backgroundImageView.frame = CGRectMake(frame.origin.x - 4, frame.origin.y - 4, frame.size.width + 8, frame.size.height + 8);
+    }
 }
 
