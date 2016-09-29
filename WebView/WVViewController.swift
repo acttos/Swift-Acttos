8e0f629055c36d687da3d4f0c97cbb60c1efaa02
Jason Ma <majinshou@gmail.com>
2016-09-29 15:48:31 +0800

    Add a new target called 'WebView' to show the usage of a UIWebView in iOS.

---
+++ WebView/WVViewController.swift
@@ -0,0 +1,27 @@
+//
+//  WVViewController.swift
+//  WebView
+//
+//  Created by Acttos on 27/9/2016.
+//  Copyright © 2016 Acttos.org. All rights reserved.
+//
+
+import UIKit
+
+class WVViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
