52fca7ebdeb5c7bcf80afbbd1d703c738f2b7255
acttos <majinshou@gmail.com>
2016-10-18 17:50:30 +0800

    Add a target for study 'FoldawayTableView'

---
+++ FoldawayTableView/FTViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoldawayTableView
+//
+//  Created by Acttos on 10/10/2016.
+//  Copyright © 2016 Acttos.org. All rights reserved.
+//
+
+import UIKit
+
+class FTViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
